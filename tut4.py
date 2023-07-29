2a4c06b4b9d060756c6f191ebc157a1e5256059a
Pardarshee <pardarsheepriyal@gmail.com>
2023-07-29 17:19:19 +0530

    adding font modification

--- tut4.py
+++ tut4.py
@@ -13,7 +13,7 @@ root.title("My GUI by Pardarshee")
 # pady - y padding
 # relief - border styling (SUNKEN, RAISED, GROOVE, RIDGE )
 
-title_label = Label(text="The sun had just risen, casting a warm \ngolden glow across the tranquil meadow. Birds chirped joyfully, \ncelebrating the beginning of a new day. A gentle breeze rustled \nthrough the leaves of the tall oak trees, creating a soothing \nmelody. As I walked along the narrow path, the scent of \nwildflowers filled the air, intoxicating my senses. In the \ndistance, a babbling brook flowed gracefully, adding to the\n serenity of the scene. Nature seemed to be waking up, welcoming me \nto embrace its beauty.", bg = "pink", fg = "black", padx = 113, pady= 94)
+title_label = Label(text="The sun had just risen, casting a warm \ngolden glow across the tranquil meadow. Birds chirped joyfully, \ncelebrating the beginning of a new day. A gentle breeze rustled \nthrough the leaves of the tall oak trees, creating a soothing \nmelody. As I walked along the narrow path, the scent of \nwildflowers filled the air, intoxicating my senses. In the \ndistance, a babbling brook flowed gracefully, adding to the\n serenity of the scene. Nature seemed to be waking up, welcoming me \nto embrace its beauty.", bg = "pink", fg = "black", padx = 113, pady= 94, font="poppins 19 bold")
 
 title_label.pack()
 root.mainloop()
